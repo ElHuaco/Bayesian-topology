78035be7c1cba0087d05650cd09026ece9c93c4a
ElHuaco <alexhuac@gmail.com>
2022-06-26 21:55:44 +0200

    All combinations implemented. Onto optimizing.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 R implementation of the K2 algorithm for learning the topology of Bayesian belief networks.
 
 ## TODO:
-<<<<<<< HEAD
+0. Reimplement correctly K2
 1. Fix add parent possibilities one by one:
 	In a network of 4 nodes we want to check respectively
 	- none for 1st
@@ -11,7 +11,8 @@ R implementation of the K2 algorithm for learning the topology of Bayesian belie
 	- none, 1, 2, (1,2), 3, (1,3), (2,3), (1,2,3) for 4th
 2. Add return of relative probability for a structure, test best structure with
 random order of the nodes.
-3. Compare computation time of normal implementation with implementation of log
+3. Use monte carlo sampling with a known joint distribution of many variables to check that we return the highest relative probability to the correct structure.
+4. Compare computation time of normal implementation with implementation of log
 probability which turns products into sums.
-4. Implement the same with ``bnstruct`` and compare relative probabilities found,
+5. Implement the same with ``bnstruct`` and compare relative probabilities found,
 as well as computation time.
--- src/K2.R
+++ src/K2.R
@@ -1,66 +1,70 @@
-# just gives back the counts of the unique possibilities
-alpha <- function(D, i, pi){
-
-    return(as.data.frame(table(D[,pi]))$Freq)
+# == all_possible_values ==
+# List of parameters:
+# - nodes: list of nodes
+# - v: possible values each node can take
+# Return Values:
+# - A dataframe with all possible combinations of node values
+all_possible_values <- function(nodes, v) {
+  npar <- rep(list(v), length(nodes))
+  return(expand.grid(npar))
 }
 
-# == Nij ==
-# computes Nij for any given parent nodes also dont matter if node itself is given or not
+# == Nijk ==
 # List of parameters:
 # - D: dataframe of joint probability distributions measured.
-# - i: index of the node at which to compute Nij.
 # - parent_choice: list of chosen parent nodes for this particular permutation.
-# Return values:
-# - Nij: sum of Nijk, as described by F. Cooper and Herskovits on the /docs/
-Nij <- function(D, i, parent_choice){
-    parent_choice <- sort(parent_choice)
-	# looks complicated because if the dataframe has only one line it turns to
-	# a vector so some format changing had to be done
-    parent_choice_new <- parent_choice[!parent_choice %in% i]
-    j <- unique(D[,parent_choice[!parent_choice %in% i]])
-    N <- c()
-    if(length(parent_choice) == 1 && parent_choice == i){
-        return(length(D[,parent_choice]))
-    }
-    else if (length(parent_choice) == 1 && parent_choice != i) {
-       parent_choice <- sort(c(parent_choice, i))
-    }
-    if(NCOL(j) == 1){
-        j <- as.data.frame(j)
-    }
-    for(i in (1:nrow(j))){
-        s <- as.numeric(j[i,])
-        c <- D
-        for(l in 1:length(parent_choice_new)){
-			#[,parent_choice[l]:length(D)] instead?
-            c <- D[which(c[,parent_choice[l]] == s[l]),]
-            if(l == length(parent_choice_new)){
-                N <- c(N, nrow(c))
-            }
-        }
-    }
-    return(N)
+# Return Values:
+# - A list Nijk, over k, as described by F. Cooper and Herskovits on the /docs/
+Nijk <- function(D, i, parent_instantiation, parent_choice, node_values){
+	result <- c()
+	for (k in node_values){
+		data_node_at_k <- D[which(D[,i] == k),]
+		parent_cols = setkey(data.table(data_node_at_k[,parent_choice]))
+		parent_vals = setkey(data.table(parent_instantiation))
+		#print(na.omit(parent_cols[parent_vals, which=TRUE]))
+		result <- c(result,
+					length(unique(na.omit(parent_cols[parent_vals, which=TRUE]))))
+	}
+	#print(result)
+	return (result)
 }
 
 # == Probability Mass ==
 # List of parameters:
 # - D: dataframe of joint probability distributions measured.
 # - i: index of the node at which we compute the probability mass.
-# - parent_choice: list of chosen parent nodes for this particular permutation.
+# - parent_choice: list of chosen parent nodes for this particular combination.
 # Return values:
-# - P1*P2: it's f(i, pi) as described by Prof. Ruiz on the /docs/
-probability <- function(D, i, parent_choice){
-	# Remove the node itself from the possible parent_choice
-	if(!i %in% parent_choice){
-		parent_choice <- c(parent_choice, i)
+# - f(i, pi) as described by Ruiz on the /docs/
+probability_mass <- function(D, i, parent_choice){
+	# 4TODO: consider log optimization
+	v <- unique(D[,i])
+    r <- length(v)
+	if (parent_choice[[1]] == 0 && length(parent_choice) == 1){
+		#No parents -> j index is ignored
+		Ni_k <- as.data.frame(table(D[,i]))$Freq
+		Ni_ <- sum(Ni_k)
+		P1 <- factorial(r-1)/factorial(Ni_ + r - 1)
+		P2 <- prod(factorial(Ni_k)) #Product over k
+		return (P1 * P2)
 	}
-    parent_choice <- sort(unique(parent_choice))
-	# TODO: consider log optimization
-    r <- length(unique(D[,i]))
-    P1 <- prod(factorial(r - 1)/(factorial(Nij(D, i, parent_choice) + r - 1)))
-	# TODO: this should be ∏ Nijk for each k
-    P2 <-  prod(factorial(alpha(D, i, parent_choice)))
-    return (P1 * P2)
+	#Parent choice determines what possible values j can take
+	phi <- all_possible_values(parent_choice, v)
+	#print(phi)
+	result <- 1.0
+	#Product over j
+	for (row in 1:nrow(phi)){
+		#cat("Probability for node", i, "and parent vals: ")
+		#print(phi[row,])
+		#cat("Nijk's: ")
+		nijk <- Nijk(D, i, phi[row,], parent_choice, v)
+		#cat(nijk, " ")
+		#cat("\n")
+		result <- result * prod(factorial(nijk)) #Product over k
+		result <- result * factorial(r - 1) / (factorial(sum(nijk) + r - 1))
+	}
+	#cat("Resulting prob mass: ", result, "\n")
+    return (result)
 }
 
 # == K2 Algorithm ==
@@ -71,28 +75,48 @@ probability <- function(D, i, parent_choice){
 # - D: dataframe of joint probability distributions measured.
 # Return values:
 # - parents: list of parents per node.
-k2 <- function(order, u, D){
-	#TODO: feed randomized order and select the best structure
+k2 <- function(order, u, D, print_result = TRUE){
+	D <- D[, order]
+	# 3TODO: feed randomized order and select the best structure
 	#  add relative probability of that structure as a return value in that case
     N <- length(D)
-    parents <- rep(list(0), length(D))
-	# The first node does not have parents.
-	parents[[1]] = integer(0)
+    parents <- rep(list(numeric(u)), N)
+	# 2TODO: add one by one instead of checking every single permutation
     for(i in 2:N){
-		# Permutation of current possible parents.
-		# Use min(i, u) because of max number of parents allowed.
-		# TODO: consider adding parents one by one instead of permutations
-		pii <- t(combn(order[1:i], min(i, u)))
-		P_old <- 0
-	    for(s in 1:nrow(pii)){
-			# Computes probability for each possible parent permutation
-	        P_new <- probability(D, i, as.numeric(pii[s,]))
-	        if(P_new > P_old){
+		num_comb = 2 ** (i - 1)
+		parent_comb <- list(0)
+		for (k in 1:(i-1)){
+			parent_comb <- c(parent_comb, combn(1:(i-1), k, simplify=FALSE))
+		}
+		P_old <- 0.0
+		for (k in 1:num_comb)
+		{
+			P_new <- probability_mass(D, i, parent_comb[[k]])
+	        if (P_new > P_old){
 	            P_old <- P_new
-	            parents[[i]] <- pii[s,][!pii[s,] %in% i]
+				parents[[i]] <- parent_comb[[k]]
 	        }
 	    }
-    print(paste('Parents of Node ', i, ' : Node =', parents[[i]]))
     }
-	return (parents)
+	#Remove '0's from parents
+	for (i in 1:N){
+		parents[i] <- lapply(parents[i], function(x) {x[x!=0]})
+	}
+	#Apply order to results
+	ordered_parents <- rep(list(numeric(u)), N)
+	for (i in 1:N){
+		ordered_parents[i] <- lapply(parents[i],
+									 function(x) {return (order[x])})
+	}
+	ordered_parents <- ordered_parents[order]
+	#Print parents per node
+	if (print_result){
+		for (i in 1:N){
+			cat(noquote(paste("Parents of node ", i, ":\t", end=" ")))
+			cat(noquote(paste("", as.character(ordered_parents[[i]]), "",
+								collapse=", ",sep="")))
+			cat("\n")
+		}
+	}
+	return (ordered_parents)
 }
