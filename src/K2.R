155a07985b582d169ac9e796ed9b09065e935862
ElHuaco <alexhuac@gmail.com>
2022-06-26 22:50:56 +0200

    Optimization of parents one by one done. Onto log optimization,.

--- src/K2.R
+++ src/K2.R
@@ -33,14 +33,15 @@ Nijk <- function(D, i, parent_instantiation, parent_choice, node_values){
 # List of parameters:
 # - D: dataframe of joint probability distributions measured.
 # - i: index of the node at which we compute the probability mass.
-# - parent_choice: list of chosen parent nodes for this particular combination.
+# - parents: list of chosen parent nodes so far.
 # Return values:
 # - f(i, pi) as described by Ruiz on the /docs/
-probability_mass <- function(D, i, parent_choice){
+probability_mass <- function(D, i, parents){
 	# 4TODO: consider log optimization
+#	print(paste("Testing parents:", parents, "on node", i))
 	v <- unique(D[,i])
     r <- length(v)
-	if (parent_choice[[1]] == 0 && length(parent_choice) == 1){
+	if (parents[[1]] == 0 && length(parents) == 1){
 		#No parents -> j index is ignored
 		Ni_k <- as.data.frame(table(D[,i]))$Freq
 		Ni_ <- sum(Ni_k)
@@ -49,22 +50,38 @@ probability_mass <- function(D, i, parent_choice){
 		return (P1 * P2)
 	}
 	#Parent choice determines what possible values j can take
-	phi <- all_possible_values(parent_choice, v)
+	phi <- all_possible_values(parents, v)
 	#print(phi)
 	result <- 1.0
 	#Product over j
 	for (row in 1:nrow(phi)){
 		#cat("Probability for node", i, "and parent vals: ")
 		#print(phi[row,])
-		#cat("Nijk's: ")
-		nijk <- Nijk(D, i, phi[row,], parent_choice, v)
-		#cat(nijk, " ")
-		#cat("\n")
+#		cat("Nijk's: ")
+		nijk <- Nijk(D, i, phi[row,], parents, v)
+#		cat(nijk, " ")
+#		cat("\n")
 		result <- result * prod(factorial(nijk)) #Product over k
 		result <- result * factorial(r - 1) / (factorial(sum(nijk) + r - 1))
 	}
 	#cat("Resulting prob mass: ", result, "\n")
     return (result)
+}
+
+maximize_probability_mass <- function(D, i, parents){
+	best_node <- 0
+	P_max <- 0.0
+	for (node in 1:(i-1)){
+		if (length(parents[parents==node]) != 0){
+			next
+		}
+		P_new <- probability_mass(D, i, c(parents, node))
+		if (P_new > P_max){
+			P_max <- P_new
+			best_node <- node
+		}
+	}
+	return (data.frame(z = c(best_node), P_new = c(P_max)))
 }
 
 # == K2 Algorithm ==
@@ -80,28 +97,27 @@ k2 <- function(order, u, D, print_result = TRUE){
 	# 3TODO: feed randomized order and select the best structure
 	#  add relative probability of that structure as a return value in that case
     N <- length(D)
-    parents <- rep(list(numeric(u)), N)
+    parents <- rep(list(0), N)
 	# 2TODO: add one by one instead of checking every single permutation
     for(i in 2:N){
-		num_comb = 2 ** (i - 1)
-		parent_comb <- list(0)
-		for (k in 1:(i-1)){
-			parent_comb <- c(parent_comb, combn(1:(i-1), k, simplify=FALSE))
-		}
-		P_old <- 0.0
-		for (k in 1:num_comb)
+		P_old <- probability_mass(D, i, parents[[i]])
+#		print(paste("P_old:", P_old))
+		parents[i] <- lapply(parents[i], function(x) {x[x!=0]})
+		while (length(parents[[i]]) < u)
 		{
-			P_new <- probability_mass(D, i, parent_comb[[k]])
-	        if (P_new > P_old){
-	            P_old <- P_new
-				parents[[i]] <- parent_comb[[k]]
+			results <- maximize_probability_mass(D, i, parents[[i]])
+#			print(paste("P_new:", results$P_new))
+	        if (results$P_new > P_old){
+	            P_old <- results$P_new
+				parents[[i]] <- c(parents[[i]], results$z)
+#				print(paste("Length parents now:", length(parents[[i]])))
 	        }
+			else{
+#				print(paste("Didn't reach a better result from", parents[[i]]))
+				break
+			}
 	    }
     }
-	#Remove '0's from parents
-	for (i in 1:N){
-		parents[i] <- lapply(parents[i], function(x) {x[x!=0]})
-	}
 	#Apply order to results
 	ordered_parents <- rep(list(numeric(u)), N)
 	for (i in 1:N){
